a64fa31f2ba1bd31e84bde099f74dc9f720c9450
Ilias May <ilias.ennmouri@mac.com>
2017-04-01 14:57:50 +0200

    Add american morse standard

--- morse/Morse.swift
+++ morse/Morse.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-let alphaToMorse = [
+let alpha2ITU = [
     "A": ".-",
     "B": "-...",
     "C": "-.-.",
@@ -71,12 +71,87 @@ let alphaToMorse = [
     "8": "---..",
     "9": "----.",
     "0": "-----",
-    " ": "/",
+    " ": " ",
+    "@": "@"
+]
+
+let alpha2morse = [
+    "A": ".-",
+    "B": "-...",
+    "C": ".. .",
+    "D": "-..",
+    "E": ".",
+    "F": ".-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": "-.-.",
+    "K": "-.-",
+    "L": "--",
+    "M": "- -",
+    "N": "-.",
+    "O": ". .",
+    "P": ".....",
+    "Q": "..-.",
+    "R": ". ..",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": ".-..",
+    "Y": ".. ..",
+    "Z": "... .",
+    "a": ".-",
+    "b": "-...",
+    "c": ".. .",
+    "d": "-..",
+    "e": ".",
+    "f": ".-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": "-.-.",
+    "k": "-.-",
+    "l": "--",
+    "m": "- -",
+    "n": "-.",
+    "o": ". .",
+    "p": ".....",
+    "q": "..-.",
+    "r": ". ..",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": ".-..",
+    "y": ".. ..",
+    "z": "... .",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----",
+    " ": " ",
     "@": "@"
 ]
 
 func convertLetterToMorse(input: Character) -> String {
-    var returnChar = alphaToMorse[String(input)]
+    var returnChar = alpha2morse[String(input)]
+    if returnChar == nil {
+        returnChar = ""
+    }
+    return returnChar!
+}
+
+func convertLetterToITU(input: Character) -> String {
+    var returnChar = alpha2ITU[String(input)]
     if returnChar == nil {
         returnChar = ""
     }
@@ -88,6 +163,18 @@ func convertStringToMorse(input: String) -> String {
     var returnString = ""
     for char in stringToConvert.characters {
         let returnChar = convertLetterToMorse(input: char)
+        if returnChar != "" {
+            returnString += returnChar + " "
+        }
+    }
+    return returnString
+}
+
+func convertStringToITU(input: String) -> String {
+    let stringToConvert = input
+    var returnString = ""
+    for char in stringToConvert.characters {
+        let returnChar = convertLetterToITU(input: char)
         if returnChar != "" {
             returnString += returnChar + " "
         }
